25634819542f0959b0d2265e64ec4fa99a9c2a35
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-16 01:17:17 +0500

    Add navLinks constant for navigation structure

---
+++ constants/navLinks.ts
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'Discover',
+        path: '/discover',
+    },
+    {
+        name: 'About the Author',
+        path: '/about-the-author',
+    },
+];
