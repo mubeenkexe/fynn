d58e641ef15cbe5cc2b79b9f7c43cf091e084089
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-16 01:16:08 +0500

    Add Button component with styles

---
+++ components/common/button/Button.tsx
@@ -0,0 +1,21 @@
+import style from './Button.module.css';
+
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+    type: 'button' | 'submit' | 'reset';
+    className?: string;
+}
+const Button = ({ children, onClick, type, className }: ButtonProps) => {
+    return (
+        <button
+            onClick={onClick}
+            type={type}
+            className={`${style.button} ${className}`}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
