51aa861452bec8565c99f134f3908c3e77ced33a
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-16 18:26:38 +0500

    Enhance Tailwind CSS configuration for improved styling and responsiveness
    - Added dark mode support with 'class' strategy.
    - Extended theme configuration to include custom border radius values.
    - Integrated 'tailwindcss-animate' plugin for enhanced animations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,28 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+    darkMode: ['class'],
     content: [
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
         './components/**/*.{js,ts,jsx,tsx,mdx}',
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {
-            colors: {
-                black: 'var(--black)',
-                lavender: 'var(--lavender)',
-                peach: 'var(--peach)',
-            },
-            fontFamily: {
-                playfairDisplay: 'var(--font-playfair-display)',
-            },
-        },
+    	extend: {
+    		colors: {
+    			black: 'var(--black)',
+    			lavender: 'var(--lavender)',
+    			peach: 'var(--peach)'
+    		},
+    		fontFamily: {
+    			playfairDisplay: 'var(--font-playfair-display)'
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
