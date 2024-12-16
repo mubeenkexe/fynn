06179eed7a8bef61b6e78d116c50481873cb8f56
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-16 18:29:30 +0500

    Add constants for navigation links and quotes
    - Introduced a new constants file to centralize navigation links and quotes for the application.
    - Removed the outdated navLinks file to streamline the codebase and improve maintainability.

---
+++ constants/index.ts
@@ -0,0 +1,21 @@
+export const NAVLINKS = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'Discover',
+        path: '/discover',
+    },
+    {
+        name: 'About the Author',
+        path: '/about-the-author',
+    },
+];
+
+export const QUOTES = [
+    '“A room without books is like a body without a soul.” – Cicero',
+    '“Books are a uniquely portable magic.” – Stephen King',
+    '“Until I feared I would lose it, I never loved to read. One does not love breathing.” – Harper Lee',
+    '“There is no friend as loyal as a book.” – Ernest Hemingway',
+];
