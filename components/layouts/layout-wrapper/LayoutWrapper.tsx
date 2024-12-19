d9972c003f7e30a01520bac27a7aea76998c18d8
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-19 15:33:25 +0500

    Refactor LayoutWrapper component to use cn utility for applying class names

--- components/layouts/layout-wrapper/LayoutWrapper.tsx
+++ components/layouts/layout-wrapper/LayoutWrapper.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { cn } from '@/lib/utils';
 
 interface LayoutWrapperProps {
     children: React.ReactNode;
     className?: string;
 }
 
+/**
+ * LayoutWrapper component that wraps its children with a div element.
+ * It applies the provided className along with a default 'layout' class.
+ *
+ * @param {LayoutWrapperProps} props - The properties for the LayoutWrapper component.
+ * @param {React.ReactNode} props.children - The child elements to be wrapped.
+ * @param {string} [props.className] - Additional class names to apply to the wrapper div.
+ *
+ * @returns {JSX.Element} The wrapped children elements within a div.
+ */
 const LayoutWrapper = ({ children, className }: LayoutWrapperProps) => {
-    return <div className={`${className} layout`}>{children}</div>;
+    return <div className={cn(className, 'layout')}>{children}</div>;
 };
 
 export default LayoutWrapper;
