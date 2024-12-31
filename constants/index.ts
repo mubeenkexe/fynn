eea8fee74f1f16ad7c489c8c084a7c2df0d8fb47
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-31 17:06:53 +0500

    Refactor constants in index.ts for consistency and readability
    - Standardized string quotes from single to double for NAV_LINKS, QUOTES, and DUMMY_FEATURED_BOOK properties.
    - Improved formatting of the DUMMY_FEATURED_BOOK description for better readability.

--- constants/index.ts
+++ constants/index.ts
@@ -1,35 +1,36 @@
 export const NAV_LINKS = [
     {
-        name: 'Home',
-        path: '/',
+        name: "Home",
+        path: "/",
     },
     {
-        name: 'Discover',
-        path: '/discover',
+        name: "Discover",
+        path: "/discover",
     },
     {
-        name: 'About the Author',
-        path: '/about-the-author',
+        name: "About the Author",
+        path: "/about-the-author",
     },
 ];
 
 export const QUOTES = [
-    '“A room without books is like a body without a soul.” – Cicero',
-    '“Books are a uniquely portable magic.” – Stephen King',
-    '“Until I feared I would lose it, I never loved to read. One does not love breathing.” – Harper Lee',
-    '“There is no friend as loyal as a book.” – Ernest Hemingway',
+    "“A room without books is like a body without a soul.” – Cicero",
+    "“Books are a uniquely portable magic.” – Stephen King",
+    "“Until I feared I would lose it, I never loved to read. One does not love breathing.” – Harper Lee",
+    "“There is no friend as loyal as a book.” – Ernest Hemingway",
 ];
 
 export const DUMMY_FEATURED_BOOK = {
-    poster: 'https://englishbooks.hr/cdn/shop/files/goosebumps-horrorland-revenge-of-the-living-dummy-book-1-r-l-stine-english-books-webshop.jpg?v=1729343237',
+    poster: "https://englishbooks.hr/cdn/shop/files/goosebumps-horrorland-revenge-of-the-living-dummy-book-1-r-l-stine-english-books-webshop.jpg?v=1729343237",
     rating: 4,
-    title: 'Cassandra',
-    author: 'Furqan Hussain',
-    description: 'Junaid, a solitary and introverted college student, stumbles upon an unexpected message on a chat app, breaking the monotony of his isolated routine. The sender, "Cassandra," appears mysterious, drawing Junaid into an uncertain yet thrilling connection. As Junaid balances the weight of his loneliness with the allure of this new interaction, he embarks on a journey that could unravel the secrets of both his heart and Cassandra\'s identity.\n Will this digital friendship evolve into something profound, or will it fade into the vastness of the virtual world?',
-    tagline: 'An enigmatic connection sparks a journey of self-discovery.',
-    publicationDate: '2023-01-01',
-    genre: 'Friendship, Love',
-    language: 'English',
+    title: "Cassandra",
+    author: "Furqan Hussain",
+    description:
+        'Junaid, a solitary and introverted college student, stumbles upon an unexpected message on a chat app, breaking the monotony of his isolated routine. The sender, "Cassandra," appears mysterious, drawing Junaid into an uncertain yet thrilling connection. As Junaid balances the weight of his loneliness with the allure of this new interaction, he embarks on a journey that could unravel the secrets of both his heart and Cassandra\'s identity.\n Will this digital friendship evolve into something profound, or will it fade into the vastness of the virtual world?',
+    tagline: "An enigmatic connection sparks a journey of self-discovery.",
+    publicationDate: "2023-01-01",
+    genre: "Friendship, Love",
+    language: "English",
     pages: 350,
-    characters: 'Junaid, Cassandra',
+    characters: "Junaid, Cassandra",
 };
