833a85fa099ab3e3429ed1a3fa2143c364bf920d
Mubeen K. <mubeenk.exe@gmail.com>
2024-12-31 17:07:32 +0500

    Refactor Button component to support navigation and improve click handling
    - Added `href` prop to the Button component to enable navigation using Next.js router.
    - Updated click handling to conditionally navigate or execute the provided onClick function.
    - Standardized string quotes from single to double for consistency.

--- components/common/button/Button.tsx
+++ components/common/button/Button.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import style from './Button.module.css';
+"use client";
+
+import React from "react";
+import style from "./Button.module.css";
+import { useRouter } from "next/navigation";
 
 interface ButtonProps {
     children: React.ReactNode;
     onClick?: () => void;
-    type: 'button' | 'submit' | 'reset';
+    type: "button" | "submit" | "reset";
     className?: string;
+    href?: string;
 }
-const Button = ({ children, onClick, type, className }: ButtonProps) => {
+const Button = ({ children, onClick, type, className, href }: ButtonProps) => {
+    const router = useRouter();
+    const handleClick = () => {
+        if (href) {
+            router.push(href);
+        } else if (onClick) {
+            onClick();
+        }
+    };
     return (
         <button
-            onClick={onClick}
+            onClick={handleClick}
             type={type}
             className={`${style.button} ${className}`}
         >
